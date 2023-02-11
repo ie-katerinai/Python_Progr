b472ce26acb0bcda370b36b4ae11d401a27a860e
ie-katerinai <117760910+ie-katerinai@users.noreply.github.com>
2023-02-12 01:44:07 +0300

    Modif task 5

--- HW_Progr/HomeWork_4/Task_5.py
+++ HW_Progr/HomeWork_4/Task_5.py
@@ -2,33 +2,34 @@
 
 
 def read_file(file_name):
-    # list_1 = []
     f = open('/Users/evahome/Documents/Python_Progr/HW_Progr/HomeWork_4/'+ file_name, 'r')
     for line in f:
         result = str(line)
     return result
 
 def split_string(initial_string):
-    new_list = initial_string.split('+')
-    return new_list
+    list_1 = initial_string.split('+')
+    list_2=[]
+    for i in range(len(list_1)):
+        list_2.append(list_1[i].strip())
+    list_3 = list_2[len(list_2)-1].split()
+    list_2[len(list_2)-1] = list_3[0]
+    list_4 = []
+    for i in range(len(list_2)):
+        list_4.append(list_2[i].split('*'))
+    return list_4
 
-# def find_x(initial_string):
-#     new_list = initial_string.split('x')
-#     return new_list
-
-def find_sign(initial_list):
-    new_list = []
-
-    for i in range(len(initial_list)):
-        if (i == '-'):
-            new_list.append('-' + i + 1)
-        else: new_list.append(i)
+def summa_multiple(str_1, str_2):
+    result = []
+    for i in range(len(str_1)):
+        result.append(str_1[i,0]+str_2[i, 0])
+    return result
 
 str_1 = 'File_1.txt'
 str_2 = 'File_2.txt'
 f = str(read_file(str_1))
 k = str(read_file(str_2))
 print(split_string(f), split_string(k))
-print(find_sign(f))
+print(summa_multiple(split_string(f), split_string(k)))
 
 
