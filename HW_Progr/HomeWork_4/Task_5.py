3bb8a487b619339411dfd18c46f2e6d62b9c0af3
ie-katerinai <117760910+ie-katerinai@users.noreply.github.com>
2023-02-14 18:23:39 +0300

    Mod task 5

--- HW_Progr/HomeWork_4/Task_5.py
+++ HW_Progr/HomeWork_4/Task_5.py
@@ -17,19 +17,20 @@ def split_string(initial_string):
     list_4 = []
     for i in range(len(list_2)):
         list_4.append(list_2[i].split('*'))
+    print(list_4)
     return list_4
 
-def summa_multiple(str_1, str_2):
-    result = []
-    for i in range(len(str_1)):
-        result.append(str_1[i,0]+str_2[i, 0])
-    return result
+# def summa_multiple(str_1, str_2):
+#     result = ''
+#     for i in range(len(str_1)):
+        
+#     return result
 
 str_1 = 'File_1.txt'
 str_2 = 'File_2.txt'
 f = str(read_file(str_1))
 k = str(read_file(str_2))
 print(split_string(f), split_string(k))
-print(summa_multiple(split_string(f), split_string(k)))
+#print(summa_multiple(split_string(f), split_string(k)))
 
 
